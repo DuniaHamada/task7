0202c234f19e595236e0946a1580877aa121fca9
DuniaHamada <121504076+DuniaHamada@users.noreply.github.com>
2024-02-18 21:50:38 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll(".box button");
+const result = document.querySelectorAll(".box span");
+const counter = [0, 0, 0, 0];
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = () => {
+    counter[i]++;
+    result[i].textContent = counter[i];
+    audio.play();
+  };
+}
